67a7321c68e27eca224daabb1ab46f27749552bd
jtsombeck@gmail.com <jtsombeck@gmail.com>
2021-11-12 18:15:23 -0600

    fixed scrambling bug where data was not un-scrambled before error was computed

--- please help/pca_interpolation.m
+++ please help/pca_interpolation.m
@@ -1,5 +1,6 @@
-function recon = pca_interpolation(data)
+function [recon,pca_params] = pca_interpolation(data)
 
 [coeff,score,~,~,~,mu] = pca(data,'algorithm','als','NumComponents',7);
 
 recon = score * coeff' + repmat(mu,length(data),1);
+pca_params = struct('coeff',coeff,'score',score,'mu',mu);
